fe1e3572b510ab9d51098485c6d7eec965608558
Anianias7 <anna.skrzeszewska95@gmail.com>
2020-10-16 18:09:46 +0200

    Add libs and improve config

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended" // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    quotes: [2, "single", "avoid-escape"]
   }
 };
